5907cd239bbca9fddec0bad73a1210af23430e4f
bohoomil <bohoomil@gmail.com>
2013-04-11 15:26:04 +0200

    minor clean up

--- dwm/addons.c
+++ dwm/addons.c
@@ -1,161 +1,161 @@
 /* bstack */
 static void
 bstack(Monitor *m) {
-	int w, h, mh, mx, tx, ty, tw;
-	unsigned int i, n;
-	Client *c;
+  int w, h, mh, mx, tx, ty, tw;
+  unsigned int i, n;
+  Client *c;
 
-	for(n = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), n++);
-	if(n == 0)
-		return;
-	if(n > m->nmaster) {
-		mh = m->nmaster ? m->mfact * m->wh : 0;
-		tw = m->ww / (n - m->nmaster);
-		ty = m->wy + mh;
-	} 
-	else {
-		mh = m->wh;
-		tw = m->ww;
-		ty = m->wy;
-	}
-	for(i = mx = 0, tx = m->wx, c = nexttiled(m->clients); c; c = nexttiled(c->next), i++) {
-		if(i < m->nmaster) {
-			w = (m->ww - mx) / (MIN(n, m->nmaster) - i);
-			resize(c, m->wx + mx, m->wy, w - (2 * c->bw), mh - (2 * c->bw), False);
-			mx += WIDTH(c);
-		} 
-		else {
-			h = m->wh - mh;
-			resize(c, tx, ty, tw - (2 * c->bw), h - (2 * c->bw), False);
-			if(tw != m->ww)
-				tx += WIDTH(c);
-		}
-	}
+  for(n = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), n++);
+  if(n == 0)
+    return;
+  if(n > m->nmaster) {
+    mh = m->nmaster ? m->mfact * m->wh : 0;
+    tw = m->ww / (n - m->nmaster);
+    ty = m->wy + mh;
+  } 
+  else {
+    mh = m->wh;
+    tw = m->ww;
+    ty = m->wy;
+  }
+  for(i = mx = 0, tx = m->wx, c = nexttiled(m->clients); c; c = nexttiled(c->next), i++) {
+    if(i < m->nmaster) {
+      w = (m->ww - mx) / (MIN(n, m->nmaster) - i);
+      resize(c, m->wx + mx, m->wy, w - (2 * c->bw), mh - (2 * c->bw), False);
+      mx += WIDTH(c);
+    } 
+    else {
+      h = m->wh - mh;
+      resize(c, tx, ty, tw - (2 * c->bw), h - (2 * c->bw), False);
+      if(tw != m->ww)
+        tx += WIDTH(c);
+    }
+  }
 }
 
 /* moveresize */
 void
 moveresize(const Arg *arg) {
-	/* only floating windows can be moved */
-	Client *c;
-	c = selmon->sel;
-	int x, y, w, h, nx, ny, nw, nh, ox, oy, ow, oh;
-	char xAbs, yAbs, wAbs, hAbs;
-	int msx, msy, dx, dy, nmx, nmy;
-	unsigned int dui;
-	Window dummy;
+  /* only floating windows can be moved */
+  Client *c;
+  c = selmon->sel;
+  int x, y, w, h, nx, ny, nw, nh, ox, oy, ow, oh;
+  char xAbs, yAbs, wAbs, hAbs;
+  int msx, msy, dx, dy, nmx, nmy;
+  unsigned int dui;
+  Window dummy;
 
-	if (!c || !arg)
-		return;
-	if (selmon->lt[selmon->sellt]->arrange && !c->isfloating)
-		return;
-	if(sscanf((char *)arg->v, "%d%c %d%c %d%c %d%c", &x, &xAbs, &y, &yAbs, &w, &wAbs, &h, &hAbs) != 8)
-		return;
-	/* compute new window position; prevent window from be positioned outside the current monitor */
-	nw = c->w + w;
-	if(wAbs == 'W')
-		nw = w < selmon->mw - 2 * c->bw ? w : selmon->mw - 2 * c->bw;
+  if (!c || !arg)
+    return;
+  if (selmon->lt[selmon->sellt]->arrange && !c->isfloating)
+    return;
+  if(sscanf((char *)arg->v, "%d%c %d%c %d%c %d%c", &x, &xAbs, &y, &yAbs, &w, &wAbs, &h, &hAbs) != 8)
+    return;
+  /* compute new window position; prevent window from be positioned outside the current monitor */
+  nw = c->w + w;
+  if(wAbs == 'W')
+    nw = w < selmon->mw - 2 * c->bw ? w : selmon->mw - 2 * c->bw;
 
-	nh = c->h + h;
-	if(hAbs == 'H')
-		nh = h < selmon->mh - 2 * c->bw ? h : selmon->mh - 2 * c->bw;
+  nh = c->h + h;
+  if(hAbs == 'H')
+    nh = h < selmon->mh - 2 * c->bw ? h : selmon->mh - 2 * c->bw;
 
-	nx = c->x + x;
-	if(xAbs == 'X') {
-		if(x < selmon->mx)
-			nx = selmon->mx;
-		else if(x > selmon->mx + selmon->mw)
-			nx = selmon->mx + selmon->mw - nw - 2 * c->bw;
-		else
-			nx = x;
-	}
+  nx = c->x + x;
+  if(xAbs == 'X') {
+    if(x < selmon->mx)
+      nx = selmon->mx;
+    else if(x > selmon->mx + selmon->mw)
+      nx = selmon->mx + selmon->mw - nw - 2 * c->bw;
+    else
+      nx = x;
+  }
 
-	ny = c->y + y;
-	if(yAbs == 'Y') {
-		if(y < selmon->my)
-			ny = selmon->my;
-		else if(y > selmon->my + selmon->mh)
-			ny = selmon->my + selmon->mh - nh - 2 * c->bw;
-		else
-			ny = y;
-	}
+  ny = c->y + y;
+  if(yAbs == 'Y') {
+    if(y < selmon->my)
+      ny = selmon->my;
+    else if(y > selmon->my + selmon->mh)
+      ny = selmon->my + selmon->mh - nh - 2 * c->bw;
+    else
+      ny = y;
+  }
 
-	ox = c->x;
-	oy = c->y;
-	ow = c->w;
-	oh = c->h;
+  ox = c->x;
+  oy = c->y;
+  ow = c->w;
+  oh = c->h;
 
-	XRaiseWindow(dpy, c->win);
-	Bool xqp = XQueryPointer(dpy, root, &dummy, &dummy, &msx, &msy, &dx, &dy, &dui);
-	resize(c, nx, ny, nw, nh, True);
+  XRaiseWindow(dpy, c->win);
+  Bool xqp = XQueryPointer(dpy, root, &dummy, &dummy, &msx, &msy, &dx, &dy, &dui);
+  resize(c, nx, ny, nw, nh, True);
 
-	/* move cursor along with the window to avoid problems caused by the sloppy focus */
-	if (xqp && ox <= msx && (ox + ow) >= msx && oy <= msy && (oy + oh) >= msy)
-	{
-		nmx = c->x - ox + c->w - ow;
-		nmy = c->y - oy + c->h - oh;
-		XWarpPointer(dpy, None, None, 0, 0, 0, 0, nmx, nmy);
-	}
+  /* move cursor along with the window to avoid problems caused by the sloppy focus */
+  if (xqp && ox <= msx && (ox + ow) >= msx && oy <= msy && (oy + oh) >= msy)
+  {
+    nmx = c->x - ox + c->w - ow;
+    nmy = c->y - oy + c->h - oh;
+    XWarpPointer(dpy, None, None, 0, 0, 0, 0, nmx, nmy);
+  }
 }
 
 /* push */
 static Client *
 prevtiled(Client *c) {
-	Client *p, *r;
+  Client *p, *r;
 
-	for(p = selmon->clients, r = NULL; p && p != c; p = p->next)
-		if(!p->isfloating && ISVISIBLE(p))
-			r = p;
-	return r;
+  for(p = selmon->clients, r = NULL; p && p != c; p = p->next)
+    if(!p->isfloating && ISVISIBLE(p))
+      r = p;
+  return r;
 }
 
 static void
 pushup(const Arg *arg) {
-	Client *sel = selmon->sel;
-	Client *c;
+  Client *sel = selmon->sel;
+  Client *c;
 
-	if(!sel || sel->isfloating)
-		return;
-	if((c = prevtiled(sel))) {
-		/* attach before c */
-		detach(sel);
-		sel->next = c;
-		if(selmon->clients == c)
-			selmon->clients = sel;
-		else {
-			for(c = selmon->clients; c->next != sel->next; c = c->next);
-			c->next = sel;
-		}
-	} else {
-		/* move to the end */
-		for(c = sel; c->next; c = c->next);
-		detach(sel);
-		sel->next = NULL;
-		c->next = sel;
-	}
-	focus(sel);
-	arrange(selmon);
+  if(!sel || sel->isfloating)
+    return;
+  if((c = prevtiled(sel))) {
+    /* attach before c */
+    detach(sel);
+    sel->next = c;
+    if(selmon->clients == c)
+      selmon->clients = sel;
+    else {
+      for(c = selmon->clients; c->next != sel->next; c = c->next);
+      c->next = sel;
+    }
+  } else {
+    /* move to the end */
+    for(c = sel; c->next; c = c->next);
+    detach(sel);
+    sel->next = NULL;
+    c->next = sel;
+  }
+  focus(sel);
+  arrange(selmon);
 }
 
 static void
 pushdown(const Arg *arg) {
-	Client *sel = selmon->sel;
-	Client *c;
+  Client *sel = selmon->sel;
+  Client *c;
 
-	if(!sel || sel->isfloating)
-		return;
-	if((c = nexttiled(sel->next))) {
-		/* attach after c */
-		detach(sel);
-		sel->next = c->next;
-		c->next = sel;
-	} else {
-		/* move to the front */
-		detach(sel);
-		attach(sel);
-	}
-	focus(sel);
-	arrange(selmon);
+  if(!sel || sel->isfloating)
+    return;
+  if((c = nexttiled(sel->next))) {
+    /* attach after c */
+    detach(sel);
+    sel->next = c->next;
+    c->next = sel;
+  } else {
+    /* move to the front */
+    detach(sel);
+    attach(sel);
+  }
+  focus(sel);
+  arrange(selmon);
 }
 
 /* togglefullscreen */
@@ -163,34 +163,34 @@ void togglefullscreen(const Arg *arg);
 
 void
 togglefullscreen(const Arg *arg) {
-	Client *c = selmon->sel;
+  Client *c = selmon->sel;
 
-	if(!c)
-		return;
-	if(!c->isfullscreen) {
-		XChangeProperty(dpy, c->win, netatom[NetWMState], XA_ATOM, 32,
-		                PropModeReplace, (unsigned char*)&netatom[NetWMFullscreen], 1);
-		c->isfullscreen = True;
-		c->oldstate = c->isfloating;
-		c->oldbw = c->bw;
-		c->bw = 0;
-		c->isfloating = True;
-		resizeclient(c, c->mon->mx, c->mon->my, c->mon->mw, c->mon->mh);
-		XRaiseWindow(dpy, c->win);
-	}
-	else
-	{
-		XChangeProperty(dpy, c->win, netatom[NetWMState], XA_ATOM, 32,
-		                PropModeReplace, (unsigned char*)0, 0);
-		c->isfullscreen = False;
-		c->isfloating = c->oldstate;
-		c->bw = c->oldbw;
-		c->x = c->oldx;
-		c->y = c->oldy;
-		c->w = c->oldw;
-		c->h = c->oldh;
-		resizeclient(c, c->x, c->y, c->w, c->h);
-		arrange(c->mon);
-	}
+  if(!c)
+    return;
+  if(!c->isfullscreen) {
+    XChangeProperty(dpy, c->win, netatom[NetWMState], XA_ATOM, 32,
+                    PropModeReplace, (unsigned char*)&netatom[NetWMFullscreen], 1);
+    c->isfullscreen = True;
+    c->oldstate = c->isfloating;
+    c->oldbw = c->bw;
+    c->bw = 0;
+    c->isfloating = True;
+    resizeclient(c, c->mon->mx, c->mon->my, c->mon->mw, c->mon->mh);
+    XRaiseWindow(dpy, c->win);
+  }
+  else
+  {
+    XChangeProperty(dpy, c->win, netatom[NetWMState], XA_ATOM, 32,
+                    PropModeReplace, (unsigned char*)0, 0);
+    c->isfullscreen = False;
+    c->isfloating = c->oldstate;
+    c->bw = c->oldbw;
+    c->x = c->oldx;
+    c->y = c->oldy;
+    c->w = c->oldw;
+    c->h = c->oldh;
+    resizeclient(c, c->x, c->y, c->w, c->h);
+    arrange(c->mon);
+  }
 }
 
